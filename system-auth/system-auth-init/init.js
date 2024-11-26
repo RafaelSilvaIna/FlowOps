03932663c5d410ac7b3fc1c1f5217bdaf89a1239
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-26 16:06:57 +0000

    2611

---
+++ system-auth/system-auth-init/init.js
@@ -0,0 +1,26 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDaffUTJnaBW7ppaHo_UQOBMHrlvMXTdGw",
+    authDomain: "flowops-7e9f0.firebaseapp.com",
+    databaseURL: "https://flowops-7e9f0-default-rtdb.firebaseio.com",
+    projectId: "flowops-7e9f0",
+    storageBucket: "flowops-7e9f0.appspot.com",
+    messagingSenderId: "541008189245",
+    appId: "1:541008189245:web:58eeb67a2507ab0bb1d754",
+    measurementId: "G-KHZ92X24C4"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        window.location.href = "dashboard.html";
+    } else {
+        console.log("Usuário não autenticado");
+    }
+});
