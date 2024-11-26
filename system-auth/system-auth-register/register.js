9d4066d94e1983553a66dceeec110af99913fceb
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-26 15:02:41 +0000

    12309

--- system-auth/system-auth-register/register.js
+++ system-auth/system-auth-register/register.js
@@ -42,7 +42,7 @@ function initBackground() {
 }
 
 
- Configuração do Three.js para o fundo 3D
+ // Configuração do Three.js para o fundo 3D
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
